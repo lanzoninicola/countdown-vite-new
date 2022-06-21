26e191fa35eebecd9b44055fa04ed5d0bfac7a63
Nicola <lanzoni.nicola@gmail.com>
2022-06-21 11:21:42 -0300

    Move the context providers up to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,42 @@ import "./i18n";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 
-import Editor from "./editor/editor";
 import Header from "./components/header/header";
 import MainContent from "./components/layout/main-content/main-content";
-import Countdowns from "./countdowns";
+import CountdownWidgetProvider from "./countdown-widget-provider/countdown-widget-provider";
+import CountdownWidgetThemeProvider from "./countdown-widget-theme-provider/countdown-widget-theme-provider";
+import useEditorSettings from "./countdown-widget/hooks/useEditorSettings";
+import Countdowns from "./countdowns/countdowns";
+import Editor from "./editor/editor";
 import FakeWrapper from "./FakeWrapper";
-import useCurrentCountdownSelector from "./countdown-widget-provider/hooks/useCurrentCountdownSelector";
-import { Countdown } from "./countdown-widget/types";
-
-interface AppProps {
-  current: Countdown | Countdown["id"] | null;
-}
 
 function App() {
-  const { currentCountdown } = useCurrentCountdownSelector();
+  const { settings, theme, isError, isLoading } = useEditorSettings({
+    isMockMode: false,
+    current: null,
+  });
 
-  const showEditor = currentCountdown && currentCountdown !== null;
+  // TODO: Error component
+  // TODO: Handling errors
+  if (isError) {
+    return <div>failed to load</div>;
+  }
+
+  // TODO: Loading component
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <FakeWrapper>
       <MainContent>
-        <Header />
-        {!showEditor && <Countdowns />}
-        {showEditor && <Editor />}
+        <CountdownWidgetProvider settings={settings}>
+          <CountdownWidgetThemeProvider theme={theme}>
+            <Header />
+            <Countdowns />
+            <Editor />
+          </CountdownWidgetThemeProvider>
+        </CountdownWidgetProvider>
       </MainContent>
     </FakeWrapper>
   );
